d4442c211e7f01ce5d87a315037904ebfbbd0ac3
AndrewPopoff <andrew.popoff@gmail.com>
2022-09-25 16:47:10 +1000

    добавили класс Point

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -9,20 +9,20 @@ namespace Tetris
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
 
-            int x1 = 2;
-            int y1 = 3;
-            char c1 = '*';
+            Point p1 = new Point();
+            p1.x = 2;
+            p1.y = 3;
+            p1.c = '*';
+            p1.Draw();
 
-            Draw(x1, y1, c1);
+            Point p2 = new Point();
+            p2.x = 12;
+            p2.y = 23;
+            p2.c = '*';
+            p2.Draw();
 
             Console.ReadKey();
         }
 
-        static void Draw(int x, int y, char c)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(c);
-
-        }
     }
 }
