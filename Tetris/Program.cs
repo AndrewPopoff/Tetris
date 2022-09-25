d6f312490f378521db7207dedd9582244e795110
AndrewPopoff <andrew.popoff@gmail.com>
2022-09-25 17:03:41 +1000

    добавили пару конструкторов

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -9,16 +9,16 @@ namespace Tetris
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
 
-            Point p1 = new Point();
-            p1.x = 2;
-            p1.y = 3;
-            p1.c = '*';
+            Point p1 = new Point(2,3,'*');
             p1.Draw();
 
-            Point p2 = new Point();
-            p2.x = 12;
-            p2.y = 23;
-            p2.c = '*';
+            Point p2 = new Point()
+            {
+                x = 12,
+                y = 23,
+                c = '*'
+            }
+            ;
             p2.Draw();
 
             Console.ReadKey();
