20283e48a2680a4e33bf6699e383338c6f149733
AndrewPopoff <andrew.popoff@gmail.com>
2022-09-27 10:44:30 +1000

    проект enums

---
+++ Enums/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day day = Day.Sun;
+
+            if (day == Day.Sun)
+                Console.WriteLine($"вск: {(int)Day.Sun}" );
+        }
+    }
+}
