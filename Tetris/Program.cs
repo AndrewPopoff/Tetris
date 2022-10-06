b2fedbe481f9589aa59ab650197a8bdd04c56c89
AndrewPopoff <andrew.popoff@gmail.com>
2022-10-06 17:06:55 +1000

    добавили критические секции но не робят

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -1,28 +1,56 @@
 ﻿using System;
 using System.Threading;
+using System.Timers;
 
 namespace Tetris
 {
     class Program
     {
         private static FigureGenerator gen = null;
+        private static Figure currentFigure = null;
+
+        const int TIMER_INTERVAL = 500;
+        static System.Timers.Timer timer;
+
+        static private Object _lockObject = new object();
+
         static void Main(string[] args)
         {
             Console.SetWindowSize(Field.Width + 1, Field.Height);
             Console.SetBufferSize(Field.Width + 1, Field.Height);
 
             gen = new FigureGenerator(Field.Width / 2, 0, Drawer.DEFAULT_SYMBOL);
-            Figure currentFigure = gen.GetNewFigure();
+            currentFigure = gen.GetNewFigure();
+
+            SetTimer();
 
             while (true)
             {
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo keyInfo = Console.ReadKey();
+                    Monitor.Enter(_lockObject);
                     var result = HandleKey(currentFigure, keyInfo);
                     ProcessResult(result, ref currentFigure);
+                    Monitor.Exit(_lockObject);
                 }
             }
+        }
+
+        private static void SetTimer()
+        {
+            timer = new System.Timers.Timer(TIMER_INTERVAL);
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
+
+        private static void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            Monitor.Enter(_lockObject);
+            var result = currentFigure.TryMove(Direction.DOWN);
+            ProcessResult(result, ref currentFigure);
+            Monitor.Exit(_lockObject);
         }
 
         private static bool ProcessResult(Result result, ref Figure currentFigure)
